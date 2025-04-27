750d755266a4f2c73f76e9d93251ac88fb1df65d
ElGael <Gaelg0203@gmail.com>
2025-04-27 09:17:01 -0600

    game-loop added
    fixed

--- Assets/enemy.cs
+++ Assets/enemy.cs
@@ -1,6 +1,4 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using System.Collections;
 
 public class ZombieFollowAndDisappear : MonoBehaviour
 {
@@ -20,8 +18,7 @@ public class ZombieFollowAndDisappear : MonoBehaviour
     {
         if (player != null && !isDead)
         {
-            Vector3 dir = (player.position - transform.position).normalized;
-            transform.position += dir * speed * Time.deltaTime;
+            float distance = Vector3.Distance(transform.position, player.position);
 
             if (distance > stoppingDistance)
             {
@@ -33,30 +30,6 @@ public class ZombieFollowAndDisappear : MonoBehaviour
                 transform.localScale = scale;
             }
         }
-    }
-
-    private IEnumerator BossSleepRoutine()
-    {
-        isSleeping = true;
-        spriteRenderer.enabled = false;
-        if (col2d != null) col2d.enabled = false;
-
-        Vector3 offScreenPos = transform.position;
-        offScreenPos.y += spawnOffsetY;
-        transform.position = offScreenPos;
-
-        yield return new WaitForSeconds(15f);
-
-        Vector3 spawnPos = player.position;
-        spawnPos.y += spawnOffsetY;
-        spawnPos.x = transform.position.x;
-        transform.position = spawnPos;
-
-        spriteRenderer.enabled = true;
-        if (col2d != null) col2d.enabled = true;
-
-        isSleeping = false;
-        bossFlag = 0;
     }
 
     void OnTriggerEnter2D(Collider2D other)
