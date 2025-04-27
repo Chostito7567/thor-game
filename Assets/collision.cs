2cdbd130d4d251be768448d43fad23d983595413
Kevin Guerra <kevin13guerra31@icloud.com>
2025-04-26 18:50:36 -0600

    trophy basics

---
+++ Assets/collision.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class DisappearOnContact : MonoBehaviour
+{
+    public int hitCounter = 3;
+    // This function will be called when the object enters the trigger zone
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        hitCounter -= 1;
+        if(hitCounter == 0){
+            Destroy(gameObject);
+        }
+        Destroy(other.gameObject);
+    }
+}
